2e9cef9988bb43a9a112d3933edc7f86863a57da
Coleman <coleman.broaddus@gmail.com>
2026-09-14 19:41:41 -0400

    formatting

--- examples/mouse_miaoconfigs.py
+++ examples/mouse_miaoconfigs.py
@@ -1,22 +1,20 @@
 import lmd_catalog as lmd
 from miao.config import MiaoConfig
 
-def build_mouse_config() -> MiaoConfig:
-    # 1. Catalog provides typed VolumeConfigs (data on disk)
-    mouse_vols = lmd.find(organism="Mouse", has_ground_truth=True)
-    volume_configs = [v.to_miao() for v in mouse_vols]
+# 1. Catalog provides typed VolumeConfigs (data on disk)
+mouse_vols = lmd.find(organism="Mouse", has_ground_truth=True)
+volume_configs = [v.to_miao() for v in mouse_vols]
 
-    # 2. Consumer provides the experiment execution configuration
-    return MiaoConfig(
-        volumes=volume_configs,
-        patch_size=[104, 232, 232],
-        resolutions=[[25.0, 10.0, 10.0]],
-        samples_per_epoch=1000,
-        sampling="random",
-        output_axes="lzyx",
-    )
+# 2. Consumer provides the experiment execution configuration
+cfg = MiaoConfig(
+    volumes=volume_configs,
+    patch_size=[104, 232, 232],
+    resolutions=[[25.0, 10.0, 10.0]],
+    samples_per_epoch=1000,
+    sampling="random",
+    output_axes="lzyx",
+)
 
-if __name__ == "__main__":
-    miao_cfg = build_mouse_config()
-    miao_cfg.to_yaml("mice_with_gt.yaml") ## serialize to yaml once you've found params you like!
+# 3. Serialize to yaml once you've found params you like!
+cfg.to_yaml("mice_with_gt.yaml") 
 
